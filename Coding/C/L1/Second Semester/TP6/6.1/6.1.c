bcac677cb0433b3827ee217b640e6763d0d44543
Khalylexe <khalil.hasnaoui15@gmail.com>
2025-02-23 20:46:06 +0100

    vim commands

---
+++ Coding/C/L1/Second Semester/TP6/6.1/6.1.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+void display_arr(int T[],int n){
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d\t",T[i]);
+    }
+    printf("\n");
+}
+void enter_arr(int T[],int n){
+    printf("enter the numbers:\n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("T[%d]=",i);
+        scanf("%d",&T[i]);
+    }
+}
+float avrg(int T[],int n){
+    float result=0;
+    for (int i = 0; i < n; i++)
+    {
+        result+=T[i];
+    }
+    result/=n;
+    return result;
+}
+int main(){
+    int n;
+    printf("enter the size small or equal than 100:");
+    scanf("%d",&n);
+    while (n>100)
+    {
+        printf("please enter a positive number:");
+        scanf("%d",&n);
+    }
+    int T[n];
+    enter_arr(T,n);
+    display_arr(T,n);
+    printf("average=%f",avrg(T,n));
+
+}
---
+++ My Notes/Vim Commands.md
@@ -0,0 +1,8 @@
+# Switch Modes
+
+
+# NORMAL mode
+`h > move left`
+`l > move right`
+`k > move up`
+`j > move down`
