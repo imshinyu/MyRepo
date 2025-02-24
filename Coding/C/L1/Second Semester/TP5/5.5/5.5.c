3155ad305081c3e23e1c2f6ca6d72ef2677071b2
imshinyu <imshinyuu@gmail.com>
2025-02-24 11:51:57 +0100

    Refactor maximum function in TP6.2 to correctly find the largest value and its address; update swap function in TP5.5 to handle same address case

--- Coding/C/L1/Second Semester/TP5/5.5/5.5.c
+++ Coding/C/L1/Second Semester/TP5/5.5/5.5.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void swap(int *a,int *b){
-    int temp=*a;
-    *a=*b;
-    *b=temp;
-    // printf("a=%d,b=%d\n",*a,*b);
-    // *a=(*a)^(*b);
-    // printf("a=%d,b=%d\n",*a,*b);
-    // *b=(*a)^(*b);
-    // printf("a=%d,b=%d\n",*a,*b);
-    // *a=(*a)^(*b);
-    // printf("a=%d,b=%d\n",*a,*b);
-
+int swap(int *a,int *b){
+    printf("%p,%p\n",a,b);
+    if(a==b){
+        printf("the address is the same\n");
+        return -1;
+    }
+    else{
+    *a=(*a)^(*b);
+        *b=(*a)^(*b);
+        *a=(*a)^(*b);
+        return 0;
+    }
 }
 int main(){
     int a=12;
     int b=5;
     printf("a=%d,b=%d\n",a,b);
-    swap(&a,&a);
-    printf("a=%d,b=%d",a,b);
+    //swap(&a,&b);
+    if(swap(&a,&b)==0) printf("a=%d,b=%d",a,b);
     return 0;
 }
--- Coding/C/L1/Second Semester/TP6/6.2/6.2.c
+++ Coding/C/L1/Second Semester/TP6/6.2/6.2.c
@@ -1,14 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
-void maximum(int T[],int n, int *m){
+void maximum(int T[],int n){
     int biggest=0;
-
-    for (int i = 0; i < n; i++)
+    int indice;
+    int *addr;
+    for (int i=0; i < n; i++)
     {
-        biggest=T[i];
+        if(T[i]<T[i+1]){
+            biggest=T[i+1];
+            indice=i+1;
+            addr=&T[i+1];
+        }
     }
-    
+    printf("biggest value:%d\n",biggest);
+    printf("address of the value:%p\n",addr);
+    printf("T[%d]\n",indice);
 }
 int main(){
-
+    int n=6;
+    int T[]={1,2,23,15,9,4};
+    maximum(T,n);
+    printf("%p",&T[2]);
 }
